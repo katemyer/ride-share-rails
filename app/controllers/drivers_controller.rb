1635d4f431d3c54a4203d3d02eeccd4d6421640a
kate myer <kate.d.mangubat@gmail.com>
2020-05-07 22:16:15 -0700

    updated driver view

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,5 +1,5 @@
 class DriversController < ApplicationController
   def index
-    @drivers = Driver.all #Task is pulling data from the Tasks table in our database
+    @drivers = Driver.all #Driver is pulling data from the Drivers table in our database
   end
 end
