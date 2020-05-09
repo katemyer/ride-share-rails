572e4bb6bd5af1d19395a9cbbb8377c1c4d42831
kate myer <kate.d.mangubat@gmail.com>
2020-05-08 23:02:44 -0700

    added relationships

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,2 +1,4 @@
 class Driver < ApplicationRecord
+  # plural because many trips could be associated with this single driver
+  has_many :trips
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,7 @@
 class Trip < ApplicationRecord
+  # singular because trip belongs to only a single driver
+  belongs_to :driver
+  
+  # singular because trip belongs to only a single passenger
+  belongs_to :passenger
 end
